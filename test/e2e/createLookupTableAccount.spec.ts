055e2623a5c5302a1d473354bb568a6e42682e76
Ashish Sapkota <aacsspkt3@gmail.com>
2025-05-27 14:05:03 +0545

    feat: add end-to-end tests for address lookup table creation, extension, and listing functionalities

---
+++ test/e2e/createLookupTableAccount.spec.ts
@@ -0,0 +1,101 @@
+import { describe } from "mocha";
+
+import { web3 } from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
+
+import { getConnection, getWallets } from "../shared";
+
+describe("lookup table actions", () => {
+	const network = "devnet";
+	const connection = getConnection(network);
+	const wallet = getWallets(network)[0];
+	console.log("provider:", wallet.publicKey.toString());
+
+	let lookupTableAddressGlob: web3.PublicKey;
+
+	it("create address lookup table", async () => {
+		// use about 0.003 SOL for two txs;
+
+		const slot = await connection.getSlot();
+		const [lookupTableInst, lookupTableAddress] = web3.AddressLookupTableProgram.createLookupTable({
+			authority: wallet.publicKey, // The authority (i.e., the account with permission to modify the lookup table)
+
+			payer: wallet.publicKey, // The payer (i.e., the account that will pay for the transaction fees)
+
+			recentSlot: slot - 1, // The recent slot to derive lookup table's address
+		});
+		console.log("lookup address:", lookupTableAddress.toString());
+		lookupTableAddressGlob = lookupTableAddress;
+
+		const lbh = await connection.getLatestBlockhash();
+		const tm = new web3.TransactionMessage({
+			instructions: [lookupTableInst],
+			payerKey: wallet.publicKey,
+			recentBlockhash: lbh.blockhash,
+		});
+
+		const vtx = new web3.VersionedTransaction(tm.compileToV0Message());
+		const signed = await wallet.signTransaction(vtx);
+
+		const sig = await connection.sendRawTransaction(signed.serialize());
+		console.log("sig:", sig);
+
+		await connection.confirmTransaction({
+			signature: sig,
+			blockhash: lbh.blockhash,
+			lastValidBlockHeight: lbh.lastValidBlockHeight,
+		});
+	});
+
+	it("extends address lookup table", async () => {
+		// const lookupTable = new web3.PublicKey("HCD4FqdYayUzUPSxSswPiEo4r7rPwd8KSvf3tqYB91SL");
+		const lookupTable = new PublicKey("C4R2sL6yj7bzKfbdfwCfH68DZZ3QnzdmedE9wQqTfAAA");
+
+		const addresses = [
+			new web3.PublicKey("De31sBPcDejCVpZZh1fq8SNs7AcuWcBKuU3k2jqnkmKc"), // stake token
+			new web3.PublicKey("5Rosz64MhKQEqDfnWU9pDTsjXZ6fMwryiLgTSGiXUbxU"), // lockup
+			new web3.PublicKey("CxGWhBSj833PkjiJWhp3LiGMdQsqURkvCfigQ5Q4YNSK"), // admin
+			new web3.PublicKey("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"), // associated token program
+			new web3.PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"), // token program
+			new web3.PublicKey("11111111111111111111111111111111"), // system program
+		];
+
+		// Create an instruction to extend a lookup table with the provided addresses
+
+		const extendInstruction = web3.AddressLookupTableProgram.extendLookupTable({
+			payer: wallet.publicKey, // The payer (i.e., the account that will pay for the transaction fees)
+			authority: wallet.publicKey, // The authority (i.e., the account with permission to modify the lookup table)
+			lookupTable, // The address of the lookup table to extend
+			addresses: addresses, // The addresses to add to the lookup table
+		});
+
+		const lbh = await connection.getLatestBlockhash();
+		const mesage = new web3.TransactionMessage({
+			instructions: [extendInstruction],
+			payerKey: wallet.publicKey,
+			recentBlockhash: lbh.blockhash,
+		});
+
+		const vtx = new web3.VersionedTransaction(mesage.compileToV0Message());
+		const signedVtx = await wallet.signTransaction(vtx);
+
+		const signature = await connection.sendRawTransaction(signedVtx.serialize());
+		console.log("signature", signature);
+		await connection.confirmTransaction({
+			signature: signature,
+			blockhash: lbh.blockhash,
+			lastValidBlockHeight: lbh.lastValidBlockHeight,
+		});
+	});
+
+	it("list lookup table account addresses", async () => {
+		const lookupTable = new PublicKey("C4R2sL6yj7bzKfbdfwCfH68DZZ3QnzdmedE9wQqTfAAA");
+
+		const lookupTables = await connection.getAddressLookupTable(lookupTable);
+		const lookupTableAccount = lookupTables.value!;
+		console.log("Lookup table address: [");
+		lookupTableAccount.state.addresses.map((a) => console.log("  ", a.toString()));
+		console.log("]");
+		console.log("authority", lookupTableAccount.state.authority?.toString());
+	});
+});
